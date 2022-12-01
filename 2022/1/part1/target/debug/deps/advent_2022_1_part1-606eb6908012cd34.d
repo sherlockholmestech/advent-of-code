81d33872bc96369ef2d13982cd28775cc342f73c
Sherlock Holmes <63144250+sherlockholmestech@users.noreply.github.com>
2022-12-01 19:37:41 +0800

    Delete 2022/1/part1/target directory

--- 2022/1/part1/target/debug/deps/advent_2022_1_part1-606eb6908012cd34.d
+++
@@ -1,5 +0,0 @@
-/mnt/a1c7784f-3706-443e-864f-2ccef1dd50be/Projects/Coding/Rust/advent-of-code/2022/1/part 1/target/debug/deps/advent_2022_1_part1-606eb6908012cd34.rmeta: src/main.rs
-
-/mnt/a1c7784f-3706-443e-864f-2ccef1dd50be/Projects/Coding/Rust/advent-of-code/2022/1/part 1/target/debug/deps/advent_2022_1_part1-606eb6908012cd34.d: src/main.rs
-
-src/main.rs:
