0704a9ee7242e35be52e0518f4fee0c78f1a520f
Sherlock Holmes <sherlockholmesxp@gmail.com>
2022-12-06 21:38:05 +0800

    Added day 6

---
+++ 2022/6/part1/src/main.rs
@@ -0,0 +1,19 @@
+use std::{fs, process::exit};
+use itertools::Itertools;
+
+fn main() {
+    let input = fs::read_to_string("input.txt").unwrap();
+    let mut start: usize = 0;
+    let mut end: usize = 4;
+    for i in 1..input.len() - 3 {
+        let original_group = input[start..end].chars().collect::<Vec<char>>();
+        let original_length = original_group.len();
+        let filtered_group = original_group.into_iter().unique().collect::<Vec<char>>();
+        if filtered_group.len() == original_length {
+            println!("{}", i + 3);
+            exit(0);
+        }
+        start += 1;
+        end += 1;
+    }
+}
